4fdaca7c53644c6e8f92bac1f481424371603111
Areej-Sheikh <areejfatimasheikh25@gmail.com>
2025-03-03 21:43:40 +0530

    fixed the tailwind css bug

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-brand-100 text-4xl font-bold underline">
+        StoreIt - The only storage solution you need
+      </h1>
+    </div>
+  );
+}
